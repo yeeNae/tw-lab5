fc21dae362e894d0618f35c54417d2a671a26407
Jasiek <joannabiliczka@gmail.com>
2026-05-24 23:25:19 +0200

    fixy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './tw-lab5',  
+  base: '/tw-lab5/',  
   plugins: [
     tailwindcss(),
   ],
